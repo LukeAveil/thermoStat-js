19946653c0c4942a9a3804716f0c03219bc43641
Luke <lukeaveil@yahoo.com>
2016-06-21 18:13:45 +0100

    temp up and down added

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -6,6 +6,16 @@ describe('Thermostat', function(){
   });
 
   it('starts at 20 degrees', function(){
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it('increases temperature with the up button', function(){
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('decreases temperature with the down button', function(){
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
   });
 });
