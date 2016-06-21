515719aebc71337a8983d24be832fc066468e51a
Luke <lukeaveil@yahoo.com>
2016-06-21 18:24:53 +0100

    minimum temperture set

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,4 +1,5 @@
 function Thermostat() {
+  this.MINIMUM_TEMPERATURE = 10;
   this.temperature = 20;
 }
 
@@ -10,6 +11,14 @@ Thermostat.prototype.up = function() {
   this.temperature += 1;
 };
 
+Thermostat.prototype.isMinimumTemperature = function() {
+  return this.temperature === this.MINIMUM_TEMPERATURE;
+};
+
+
 Thermostat.prototype.down = function() {
+  if (this.isMinimumTemperature()) {
+    return;
+  }
   this.temperature -= 1;
 };
